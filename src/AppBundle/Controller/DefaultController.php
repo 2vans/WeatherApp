cb37bce6c063230d5475dc42ed5cb949296cb23f
Michał Jabłoński <mock.jablonski@gmail.com>
2017-07-25 13:44:40 +0200

    Added City and Country

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class DefaultController extends Controller
 {
@@ -13,10 +14,20 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+    /*
+        $this->add('isAttending', ChoiceType::class, array(
+            'choices'  => array(
+                'Maybe' => null,
+                'Yes' => true,
+                'No' => false,
+            ),
+        ));
+*/
+
 
 
         $BASE_URL = "http://query.yahooapis.com/v1/public/yql";
-        $yql_query = 'select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="warsaw")';
+        $yql_query = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="warsaw")';
         $yql_query_url = $BASE_URL . "?q=" . urlencode($yql_query) . "&format=json";
         // Make call with cURL
         $session = curl_init($yql_query_url);
@@ -25,7 +36,12 @@ class DefaultController extends Controller
         // Convert JSON to PHP object
         $phpObj =  json_decode($json);
 
+        $city = $phpObj->query->results->channel->location->city;
+        $country = $phpObj->query->results->channel->location->country;
+        $temp = $phpObj->query->results->channel->item->condition->temp;
+        $condition = $phpObj->query->results->channel->location->country;
+
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array('WeatherForcast' => $phpObj));
+        return $this->render('default/index.html.twig', array('city' => $city, 'country' =>$country, 'temp' => $temp, 'condition' => $condition));
     }
 }

5a18e31dac4afd58eed5e581f9bff7acfa945c8c
Michał Jabłoński <mock.jablonski@gmail.com>
2017-07-25 17:45:29 +0200

    Service but good

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,37 +6,24 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use AppBundle\Service\Weather;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/{mainCity}", name="WeatherApp")
+     * @param string $mainCity
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction($mainCity = 'warszawa')
     {
 
+        $weatherService = new Weather();
+        $currentWeather = $weatherService->getWeather($mainCity);
 
 
 
-        $BASE_URL = "http://query.yahooapis.com/v1/public/yql"; // do parametrow
-        $yql_query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='$mainCity')";
-        $yql_query_url = $BASE_URL . "?q=" . urlencode($yql_query) . "&format=json";
-        // Make call with cURL
-        $session = curl_init($yql_query_url);
-        curl_setopt($session, CURLOPT_RETURNTRANSFER,true);
-        $json = curl_exec($session);
-        // Convert JSON to PHP object
-        $phpObj =  json_decode($json);
-
-        $response = $phpObj->query->results->channel;
-
-        $city = $response->location->city;
-        $country = $response->location->country;
-        $temp = $response->item->condition->temp;
-        $condition = $response->item->condition->text;
-
-
-        return $this->render('default/index.html.twig',
-            array('phpObj' => $phpObj ,'city' => $city, 'country' =>$country, 'temp' => $temp, 'condition' => $condition));
+        return $this->render('default/index.html.twig', $currentWeather
+            );
     }
 }
--- src/AppBundle/Service/Weather.php
+++ src/AppBundle/Service/Weather.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: michal
- * Date: 25.07.17
- * Time: 15:38
- */
+    namespace AppBundle\Service;
+
+    class Weather {
+        public function getWeather($whichCity)
+        {
+            $BASE_URL = "http://query.yahooapis.com/v1/public/yql"; // do parametrow
+            $yql_query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='$whichCity')";
+            $yql_query_url = $BASE_URL . "?q=" . urlencode($yql_query) . "&format=json";
+            // Make call with cURL
+            $session = curl_init($yql_query_url);
+            curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+            $json = curl_exec($session);
+            // Convert JSON to PHP object
+            $phpObj = json_decode($json);
+
+            $response = $phpObj->query->results->channel;
+
+            $city = $response->location->city;
+            $country = $response->location->country;
+            $temp = $response->item->condition->temp;
+            $condition = $response->item->condition->text;
+
+            return array('phpObj' => $phpObj, 'city' => $city, 'country' => $country, 'temp' => $temp, 'condition' => $condition);
+
+
+        }
+    }

62f9fad98b473183222410f1687ab63f0767a4cc
Michał Jabłoński <mock.jablonski@gmail.com>
2017-07-25 14:42:28 +0200

    JSON

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,24 +10,16 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="WeatherApp")
+     * @Route("/{mainCity}", name="WeatherApp")
      */
-    public function indexAction(Request $request)
+    public function indexAction($mainCity = 'warsaw')
     {
-    /*
-        $this->add('isAttending', ChoiceType::class, array(
-            'choices'  => array(
-                'Maybe' => null,
-                'Yes' => true,
-                'No' => false,
-            ),
-        ));
-    */
+
 
 
 
         $BASE_URL = "http://query.yahooapis.com/v1/public/yql";
-        $yql_query = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="warsaw")';
+        $yql_query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='$mainCity')";
         $yql_query_url = $BASE_URL . "?q=" . urlencode($yql_query) . "&format=json";
         // Make call with cURL
         $session = curl_init($yql_query_url);
@@ -36,12 +28,15 @@ class DefaultController extends Controller
         // Convert JSON to PHP object
         $phpObj =  json_decode($json);
 
-        $city = $phpObj->query->results->channel->location->city;
-        $country = $phpObj->query->results->channel->location->country;
-        $temp = $phpObj->query->results->channel->item->condition->temp;
-        $condition = $phpObj->query->results->channel->location->country;
+        $response = $phpObj->query->results->channel;
+
+        $city = $response->location->city;
+        $country = $response->location->country;
+        $temp = $response->item->condition->temp;
+        $condition = $response->item->condition->text;
 
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array('city' => $city, 'country' =>$country, 'temp' => $temp, 'condition' => $condition));
+        return $this->render('default/index.html.twig',
+            array('phpObj' => $phpObj ,'city' => $city, 'country' =>$country, 'temp' => $temp, 'condition' => $condition));
     }
 }

b92a02a59140658111bec41f54eafc1be72d5442
Michał Jabłoński <mock.jablonski@gmail.com>
2017-07-27 16:21:52 +0200

    octrine start

--- src/AppBundle/Entity/WeatherInfo.php
+++ src/AppBundle/Entity/WeatherInfo.php
@@ -8,12 +8,42 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity;
+ * @ORM\Table(name="Weather_Info")
+ */
+
 
 class WeatherInfo
 {
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+
     private $city;
+    /**
+     * @ORM\Column(type="string")
+     */
     private $country;
+    /**
+     * @ORM\Column(type="var")
+     */
     private $temp;
+    /**
+     * @ORM\Column(type="string")
+     */
     private $condition;
 
     /**
